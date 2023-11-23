54ac84eed271ae77b5517003ad196be40ec4d5ab
Prasham Jain <prashamkjain@gmail.com>
2023-11-23 16:55:39 +0530

    C++ programs

---
+++ Prog1.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*int main(){
+    cout<< "Namaste"<<" "<<"I am"<<" learning to program"<< " ";
+    cout<<sqrt(16) << " job";
+}*/
+
+/*int main(){
+    int a = 2;
+    int b = 3;
+    int sum = a+b;
+}*/
+
+/*int main(){
+   char c = 'p';
+   int a = 124;
+   double d = 3.9;
+   a = 6.5; // a becomes 6
+   bool b = a-6; // b = 0
+   cout << c << " " << a << " " << b <<" "<< d << endl;
+}*/
+
+/*int main(){
+    long int a = 12345;
+    long b = 12346;
+    long long int c = 1234567;
+    long long d = 1234568;
+    cout << a << " " << b << " " << c << " " << d;
+}*/
+
+/*int main(){
+    //cout << 2*3 << " " << 20%7;
+    int a = 5;
+    a = a+1;
+    cout << a <<endl; //6
+    cout << a++ <<endl; //6
+    cout << a <<endl; //7
+    cout << a-- <<endl;//7
+    cout << ++a <<endl;//7
+    cout << --a <<endl;//6
+    cout << a--+a <<endl;//11
+}*/
+
+/*int main(){
+char c = 'a';
+//char d = 'ab'; invalid
+int d = c;
+cout << d << " ";
+char e = 'A';
+
+cout << (int)c << endl;
+cout << (char)d << endl;
+cout << c-e << " ";
+int f = c-e+65;
+char g = (char)f;
+cout << g << endl;
+
+}*/
+
+/*int main(){
+    int a;
+    double b;
+    char c;
+    cin >> a >> b >>c;
+    cout << a << " "<< " " << c << " "<< (int) c;
+}*/
+
+int main(){
+    int a, b, c;
+    char d;
+    cin >> d >> a;
+    cout << d << " " << a;
+
+}
